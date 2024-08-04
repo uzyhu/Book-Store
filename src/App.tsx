5dc4300147a7add24421366b1a3546c9e68d1d4b
uzyhu <dnwlgn0807@naver.com>
2024-08-04 18:43:26 +0900

    add: model & create client & category api

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,26 @@ import { ThemeName, getTheme, light } from "./style/theme";
 import ThemeSwitcher from "./components/header/ThemeSwitcher";
 import { useContext, useState } from "react";
 import { BookStoreThemeProvider, ThemeContext } from "./context/themeContext";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Error from "./components/common/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout><Home /></Layout>,
+    errorElement: <Error /> //루트 하위 모든 페이지에 적용됨
+  },
+  {
+    path: "/books",
+    element: <Layout><div>도서 목록</div></Layout>
+  }
+]);
 
 function App() {
   return (
     <BookStoreThemeProvider>
       {/* <ThemeSwitcher /> */}
-      <Layout>
-        <Home />
-      </Layout>
+        <RouterProvider router={router} />
     </BookStoreThemeProvider>
   );
 }
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,58 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import logo from '../../assets/images/logo.png';
-import {FaSignInAlt, FaRegUser} from "react-icons/fa";
-const CATEGORY = [
-  {
-    id: null,
-    name: "전체"
-  },
-  {
-    id: 0,
-    name: "동화"
-  },
-  {
-    id: 1,
-    name: "소설"
-  },
-  {
-    id: 2,
-    name: "사회"
-  }
-];
+import logo from "../../assets/images/logo.png";
+import { FaSignInAlt, FaRegUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { Category } from "../../models/category.model";
+import { fetchCategory } from "../../api/category.api";
 
 const Header = () => {
+  const [category, setCategory] = useState<Category[]>([]);
+
+  useEffect(() => {
+    fetchCategory().then((category) => {
+      setCategory(category);
+    })
+  })
+  
   return (
     <HeaderStyle>
       <h1 className="logo">
-      <img src={logo} alt="book store" />
+        <Link to="/">
+          <img src={logo} alt="book store" />
+        </Link>
       </h1>
       <nav className="category">
         <ul>
-          {
-            CATEGORY.map((item) => (
-              <li key={item.id}>
-                <a href={item.id === null ? `/books` : `/books?category_id=${item.id}`}>
-                  {item.name}
-                </a>
-              </li>
-            ))
-          }
+          {category.map((item) => (
+            <li key={item.id}>
+              <Link
+                to={
+                  item.id === null ? `/books` : `/books?category_id=${item.id}`
+                }
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
       <nav className="auth">
         <ul>
           <li>
-            <a href="/login">
+            <Link to="/login">
               <FaSignInAlt />
               로그인
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="/join">
+            <Link to="/join">
               <FaRegUser />
               회원가입
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
@@ -63,34 +60,34 @@ const Header = () => {
 const HeaderStyle = styled.header`
   width: 100%;
   margin: 0 auto;
-  max-width: ${({theme}) => theme.layout.width.large};
+  max-width: ${({ theme }) => theme.layout.width.large};
 
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 20px 0;
-  border-bottom: 1px solid ${({theme}) => theme.color.background};
+  border-bottom: 1px solid ${({ theme }) => theme.color.background};
 
   .logo {
     img {
       width: 200px;
     }
-  };
+  }
 
   .category {
     ul {
       display: flex;
       gap: 32px;
       li {
-        a{
+        a {
           font-size: 1.5rem;
           font-weight: 600;
           text-decoration: none;
 
-          color: ${({theme}) => theme.color.text};
+          color: ${({ theme }) => theme.color.text};
 
           &:hover {
-            color: ${({theme}) => theme.color.primary};
+            color: ${({ theme }) => theme.color.primary};
           }
         }
       }
